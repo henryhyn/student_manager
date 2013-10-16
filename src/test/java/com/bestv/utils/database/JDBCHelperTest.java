efc29a337c271d281b20df4f37989a8bd21e02b9
richard <hynmath9@163.com>
2013-10-17 01:06:06 +0800

    添加JDBC帮助方法

--- src/test/java/com/bestv/utils/database/JDBCHelperTest.java
+++ src/test/java/com/bestv/utils/database/JDBCHelperTest.java
@@ -3,9 +3,7 @@ package com.bestv.utils.database;
 import org.junit.Test;
 
 import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.util.ArrayList;
 
 /**
  * Created with IntelliJ IDEA.
@@ -18,26 +16,11 @@ public class JDBCHelperTest {
     @Test
     public void test() {
         String sql = "select id, name, number, class_id from student";
-        Connection connection = null;
-        Statement stmt = null;
-        ResultSet rs = null;
-        try {
-            connection = DBConnectionManager.getConnection();
-            stmt = connection.createStatement();
-            rs = stmt.executeQuery(sql);
-            while (rs.next()) {
-                System.out.println(rs.getString(2));
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                rs.close();
-                stmt.close();
-                connection.close();
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
+        Connection connection = DBConnectionManager.getConnection();
+        ArrayList<Object[]> rows = JDBCHelper.executeQuery(connection, sql);
+        for (Object[] row : rows) {
+            System.out.println(row[1]);
         }
+        JDBCHelper.close(connection);
     }
 }
