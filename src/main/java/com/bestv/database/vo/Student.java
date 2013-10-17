03c71ed78342b40ea877c7916c272ae09b2b7c45
henry <henryhyn@163.com>
2013-10-17 15:35:06 +0800

    移动 Hibernate 配置文件

--- src/main/java/com/bestv/database/vo/Student.java
+++ src/main/java/com/bestv/database/vo/Student.java
@@ -1,17 +1,22 @@
 package com.bestv.database.vo;
 
+import javax.persistence.Entity;
+
 /**
  * 节目类
  * User: henry
  * Date: 13-9-11
  * Time: 下午3:54
  */
+@Entity
 public class Student {
     private Integer id;
     private String name;
     private String number;
     private Integer classId;
 
+    @javax.persistence.Column(name = "id")
+    @javax.persistence.Id
     public Integer getId() {
         return id;
     }
@@ -20,6 +25,12 @@ public class Student {
         this.id = id;
     }
 
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @javax.persistence.Column(name = "name")
+    @javax.persistence.Basic
     public String getName() {
         return name;
     }
@@ -28,6 +39,8 @@ public class Student {
         this.name = name;
     }
 
+    @javax.persistence.Column(name = "number")
+    @javax.persistence.Basic
     public String getNumber() {
         return number;
     }
@@ -36,11 +49,37 @@ public class Student {
         this.number = number;
     }
 
+    @javax.persistence.Column(name = "class_id")
+    @javax.persistence.Basic
     public Integer getClassId() {
         return classId;
     }
 
     public void setClassId(Integer classId) {
         this.classId = classId;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Student student = (Student) o;
+
+        if (classId != null ? !classId.equals(student.classId) : student.classId != null) return false;
+        if (id != null ? !id.equals(student.id) : student.id != null) return false;
+        if (name != null ? !name.equals(student.name) : student.name != null) return false;
+        if (number != null ? !number.equals(student.number) : student.number != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        result = 31 * result + (number != null ? number.hashCode() : 0);
+        result = 31 * result + (classId != null ? classId.hashCode() : 0);
+        return result;
     }
 }
