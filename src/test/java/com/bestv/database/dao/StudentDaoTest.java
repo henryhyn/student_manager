eddaa3ce15d31d86140ae4bd2ed89b6055171ad7
henry <henryhyn@163.com>
2013-10-25 16:09:21 +0800

    测试细微调整

--- src/test/java/com/bestv/database/dao/StudentDaoTest.java
+++ src/test/java/com/bestv/database/dao/StudentDaoTest.java
@@ -1,9 +1,13 @@
 package com.bestv.database.dao;
 
 import com.bestv.database.vo.Student;
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.metadata.ClassMetadata;
 import org.junit.Test;
 
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created with IntelliJ IDEA.
@@ -18,6 +22,23 @@ public class StudentDaoTest {
         List<Student> students = DaoFactory.getStudentDao().findAll();
         for (Student student : students) {
             System.out.println(student.getName());
+        }
+
+        final Session session = DaoFactory.getStudentDao().getSession();
+        try {
+            System.out.println("querying all the managed entities...");
+            final Map metadataMap = session.getSessionFactory().getAllClassMetadata();
+            for (Object key : metadataMap.keySet()) {
+                final ClassMetadata classMetadata = (ClassMetadata) metadataMap.get(key);
+                final String entityName = classMetadata.getEntityName();
+                final Query query = session.createQuery("from " + entityName);
+                System.out.println("executing: " + query.getQueryString());
+                for (Object o : query.list()) {
+                    System.out.println("  " + o);
+                }
+            }
+        } finally {
+            session.close();
         }
     }
 }
